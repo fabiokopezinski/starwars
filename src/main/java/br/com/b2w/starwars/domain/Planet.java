c0a793030e4b7df1ba82aed6b56aba6ec2e6ddd9
Fábio Kopezinski <fabiokopezinski@gmail.com>
2021-04-10 16:02:21 -0300

    Initial commit

---
+++ src/main/java/br/com/b2w/starwars/domain/Planet.java
@@ -0,0 +1,33 @@
+package br.com.b2w.starwars.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection="planet")
+public class Planet {
+    
+    @Id
+    private String planetCode;
+
+    @Indexed(unique = true)
+    @Field(name="NOME")
+    private String name;
+
+    @Field(name="CLIMA")
+    private String climate;
+
+    @Field(name="TERRENO")
+    private String ground;
+
+    @Field(name="APARICOES_FILMES")
+    private String filmAppearances;
+
+
+}
