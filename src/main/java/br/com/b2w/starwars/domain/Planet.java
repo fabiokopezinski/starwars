a33337252170dad35e727f0d96656b862f81e54f
Fábio Kopezinski <fabiokopezinski@gmail.com>
2021-04-10 16:06:55 -0300

    initial commit

--- src/main/java/br/com/b2w/starwars/domain/Planet.java
+++ src/main/java/br/com/b2w/starwars/domain/Planet.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
+ 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
