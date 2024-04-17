755c9c91a30c71044b3ca609daa9c93fa756d05c
FABIO KOPEZINSKI CARVALHO <fabiokc@ciandt.com>
2024-04-17 11:30:02 -0300

    ajuste

--- src/main/java/br/com/b2w/starwars/controller/command/PlanetControllerCommand.java
+++ src/main/java/br/com/b2w/starwars/controller/command/PlanetControllerCommand.java
@@ -20,7 +20,7 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/planets")
 @AllArgsConstructor
 @RestController
-@Tag(name = "Planets")
+@Tag(name = "Planetas")
 public class PlanetControllerCommand {
 
     private PlanetServiceCommand command;
